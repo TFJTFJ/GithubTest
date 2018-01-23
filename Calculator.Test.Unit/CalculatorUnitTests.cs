af4595dee12c196e301244343d84fdfcfa6fe6a8
Troels Fedder Jensen <tfj@ase.au.dk>
2018-01-23 11:13:35 +0100

    Added unit test for Add()

--- Calculator.Test.Unit/CalculatorUnitTests.cs
+++ Calculator.Test.Unit/CalculatorUnitTests.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace Calculator.Test.Unit
 {
+    [TestFixture]
     public class CalculatorUnitTests
     {
+        [Test]
+        public void TestCalc1()
+        {
+            var uut = new Calculator();
+            Assert.That(uut.Add(2, 2), Is.EqualTo(4));
+        }
     }
 }
--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -8,5 +8,9 @@ namespace Calculator
 {
     public class Calculator
     {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
     }
 }
